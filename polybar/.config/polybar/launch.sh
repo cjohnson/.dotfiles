f168eaf51d678c3b5cff10e78f53e3dfd2ab1b59
cjohnson <collin.g.johnson@gmail.com>
2024-06-24 22:26:45 -0400

    Add polybar

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Terminate currently running bars
+polybar-msg cmd quit
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
+echo "Polybar bars launched..."
