4d1df885157ec05f0923f1fd1a0cfc3fb2ab65d0
cjohnson <collin.g.johnson@gmail.com>
2024-06-25 21:07:48 -0400

    Replace image-url.txt with fetching script in dotfiles root

---
+++ fetch-background.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+image_url="https://images.unsplash.com/photo-1444065707204-12decac917e8?q=80&w=1173&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
+curl -o background.jpg -O $image_url
