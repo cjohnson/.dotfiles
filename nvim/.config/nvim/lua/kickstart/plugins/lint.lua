330037919943b394be76e05a49f5e2f464dbf773
cjohnson <collin.g.johnson@gmail.com>
2024-06-26 23:26:02 -0400

    Add .stylua.toml; format directory

--- nvim/.config/nvim/lua/kickstart/plugins/lint.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lint.lua
@@ -4,7 +4,7 @@ return {
     'mfussenegger/nvim-lint',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
-      local lint = require 'lint'
+      local lint = require('lint')
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
       }
@@ -46,9 +46,7 @@ return {
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
-        callback = function()
-          require('lint').try_lint()
-        end,
+        callback = function() require('lint').try_lint() end,
       })
     end,
   },
