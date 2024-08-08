54f8b0ae44cee4de117796940e2cf29b0c33d16d
cjohnson <collin.g.johnson@gmail.com>
2024-08-07 21:21:17 -0400

    Add lazy.nvm config from LuaSnip

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -578,13 +578,8 @@ require('lazy').setup({
       -- Snippet Engine & its associated nvim-cmp source
       {
         'L3MON4D3/LuaSnip',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has('win32') == 1 or vim.fn.executable('make') == 0 then return end
-          return 'make install_jsregexp'
-        end)(),
+        run = 'v2.*',
+        build = 'make install_jsregexp',
         dependencies = {
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
